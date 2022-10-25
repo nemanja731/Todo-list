5001fe80cddda3b83a176e42c8f00822f88f6376
Nemanja Jankovic <nemanja.jankovic731@gmail.com>
2022-10-26 01:46:19 +0200

    Update making html tags in addFunction fcn

--- script.js
+++ script.js
@@ -2,15 +2,20 @@ function addFunction() {
   if(document.querySelector('#input').value.length == 0)
     alert("Please enter an item")
   else {
-    document.querySelector('.container').innerHTML += `
-    <div class="item">
-      <div class="item-text">
-        ${document.querySelector('#input').value}
-      </div>
-      <button class="item-button">
-        <i class="fa fa-trash-o" style="font-size:20px"></i>
-      </button>
-    </div>`;
+    //creating one item in the list
+    let item = document.createElement('div');
+    item.className = "item";
+    let itemText = document.createElement('div');
+    itemText.className = "item-text";
+    itemText.innerText = document.querySelector('#input').value;
+    let itemButton = document.createElement('button');
+    itemButton.className = "item-button";
+    itemButton.innerHTML = '<i class="fa fa-trash-o"></i>'
+    let container = document.querySelector('.container');
+    container.appendChild(item);
+    item.appendChild(itemText);
+    item.appendChild(itemButton);
+    //checking whether a product has been deleted from the list
     var currentTasks = document.querySelectorAll(".item-button");
     for(var i=0; i<currentTasks.length; i++) {
       currentTasks[i].onclick = function() {
